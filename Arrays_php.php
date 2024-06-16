1b24f90807d1d3f4dd726f0e85bcfb88fd2a9e42
kayov <leonardofernandes22.pb@gmail.com>
2024-06-16 14:05:07 -0300

    aula de arrays php!

---
+++ Arrays_php.php
@@ -0,0 +1,11 @@
+<?php
+
+// Create an array of fruits
+$fruits = ["Maçã", "Banana", "Laranja", "Uva", "Morango"];
+
+// Display the fruits using a loop
+foreach ($fruits as $fruit) {
+    echo "$fruit \n";
+}
+
+?>
