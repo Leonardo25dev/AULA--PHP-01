7edee641825709b3887d753568dbbe10d35a201f
kayov <leonardofernandes22.pb@gmail.com>
2024-06-16 14:27:45 -0300

    aula de estrutura de desisao switch !

---
+++ estrutura_de_decisao_switch.php
@@ -0,0 +1,33 @@
+<?php
+
+// Define the day of the week
+$day = "Domingo";
+
+// Check the day of the week and display a corresponding message
+switch ($day) {
+    case "Segunda-feira":
+        echo "Hoje é segunda-feira!";
+        break;
+    case "Terça-feira":
+        echo "Hoje é terça-feira!";
+        break;
+    case "Quarta-feira":
+        echo "Hoje é quarta-feira!";
+        break;
+    case "Quinta-feira":
+        echo "Hoje é quinta-feira!";
+        break;
+    case "Sexta-feira":
+        echo "Hoje é sexta-feira!";
+        break;
+    case "Sábado":
+        echo "Hoje é sábado!";
+        break;
+    case "Domingo":
+        echo "Hoje é domingo!";
+        break;
+    default:
+        echo "Dia inválido.";
+}
+
+?>
