618e5fb5662cafd7d2f9e9db3aff38a4dac2d437
kayov <leonardofernandes22.pb@gmail.com>
2024-06-16 13:33:17 -0300

    foi criado estrutura condicional

---
+++ estrutura_condicional.php
@@ -0,0 +1,13 @@
+<?php
+
+// Set the age variable
+$idade = 18;
+
+// Check if the age is greater than or equal to 18
+if ($idade >= 18) {
+    echo "voce e um adulto.";
+} else {
+    echo "voce nao e um adulto.";
+}
+
+?>
