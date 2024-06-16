e088b616e3a73b5f322a19a6323abf4ca86f3ded
kayov <leonardofernandes22.pb@gmail.com>
2024-06-16 14:57:54 -0300

    aula de estrutura de repeticao do while!

---
+++ estrutura_de_repeticao_do_while.php
@@ -0,0 +1,15 @@
+<?php
+
+// Set the initial counter variable
+$counter = 1;
+
+// Repeat the loop at least once, then check the condition
+do {
+    // Display the current counter value
+    echo "Número: $counter \n";
+
+    // Increment the counter
+    $counter++;
+} while ($counter <= 5); // Check the condition after each iteration
+
+?>
