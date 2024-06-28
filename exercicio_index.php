b24a55c14906c00a3e2b42157e5604fb79bbc60f
kayov <leonardofernandes22.pb@gmail.com>
2024-06-27 22:22:11 -0300

    aula exercico index  php

---
+++ exercicio_index.php
@@ -0,0 +1,11 @@
+<?php
+echo "Oi. Eu serei visto na sua tela"; // Comentário de linha
+// Eu não! Sou apenas um comentário.
+
+echo "Oi, Eu também serei visto por você"; // Comentário de linha
+# Já eu não serei!
+
+echo "E eu aqui novamente na sua tela, rs"; // Comentário de linha
+// Eu não aparecerei na sua tela novamente
+// pois sou um comentário */
+?>
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <body>
 
-<h1>Estamos aprendendo PHP !< /h1>
-<? php
-//A linha com duas barras são comentários e não serão interpretadas nem exibidas
+<h1>Estamos aprendendo PHP!</h1>
 
-//0 comando echo escreve no navegador o conteúdo que está dentro das aspas
-"Vamos prosseguir aprendendo PHP";
+<?php //A linha com duas barras são comentários e não serão interpretadas nem exibidas
 
-echo
+//O comando echo escreve no navegador o conteúdo que está dentro das aspas
+echo "Vamos prosseguir aprendendo PHP";
+
+?>
 
 </body>
 </html>
