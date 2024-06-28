6822eaac95d4a57511ba012fa63128651f7cdf86
kayov <leonardofernandes22.pb@gmail.com>
2024-06-27 22:50:48 -0300

    aula estrutura de repeticao while  php

---
+++ estrutura_repeticao_while.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$num = 0;
+
+while ($num < 10) {
+    echo $num; // Print the current value of $num
+    $num++; // Increment $num
+}
+?>
+
+</body>
+</html>
