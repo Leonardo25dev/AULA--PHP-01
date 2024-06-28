7c28cc098d8bd12ee3460bdba5996663e9d2f554
kayov <leonardofernandes22.pb@gmail.com>
2024-06-27 22:33:31 -0300

    aula operadores de atribuiçao  php

---
+++ operadores_atribuicao.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a = 1; // Initialize $a to 1
+
+// Increment $a by 2
+$a += 2;
+echo $a; // Output: 3
+
+// Subtract 2 from $a
+$a -= 2;
+echo $a; // Output: 1
+
+// Multiply $a by 2
+$a *= 2;
+echo $a; // Output: 2
+
+// Divide $a by 2
+$a /= 2;
+echo $a; // Output: 1
+
+// Increment $a by 1 and return the value
+echo ++$a; // Output: 2
+
+// Return the value of $a and then increment by 1
+echo $a++; // Output: 2
+
+// Decrement $a by 1 and return the value
+echo --$a; // Output: 1
+
+// Return the value of $a and then decrement by 1
+echo $a--; // Output: 1
+?>
+
+</body>
+</html>
